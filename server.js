2ec75a51fe30d56874f330362585671e1ee3f1d2
Jesus Moreno Amor <jesus@morenoamor.com>
2019-03-22 11:51:14 +0100

    Heroku deploy

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './src/index.html'));
+    res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 app.listen(process.env.PORT || 8080);
